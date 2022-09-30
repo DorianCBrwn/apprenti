f2a9b8e814ebd02df997d73c7841354e61cc3258
Dorian C Brown <brown.3794@gmail.com>
2022-09-29 21:59:58 -0400

    feat: :sparkles: Convert Navbar links into Menu component

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,20 +1,21 @@
 <script>
 	import { page } from '$app/stores';
 	import { Hamburger } from 'svelte-hamburgers';
-	import Logo from '$lib/components/icons/apprenti-logo.svelte'
+	import Menu from '$lib/components/Menu.svelte';
+	import { fly } from 'svelte/transition';
+	import Logo from '$lib/components/icons/apprenti-logo.svelte';
 
-	/**
-	 * @param {boolean} open
-	 */
-	let open;
+	let open = false;
 </script>
 
 <!-- Mobile Nav  -->
-<div class="fixed right-6 top-6">
+<div class="fixed right-6 top-6 md:hidden" >
 	<Hamburger --color="rgb(7, 129, 190)" bind:open />
 </div>
 
-<header class="col-span-3 text-center bg-slate-500">
+<header
+	class="absolute w-full h-screen  md:relative md:col-span-3 text-center bg-slate-500 -translate-x-full md:translate-x-0"
+>
 	<div class="my-10">
 		<a href="/">
 			<Logo />
@@ -22,33 +23,8 @@
 	</div>
 
 	<nav>
-		<ul class="list-none text-2xl font-bold">
-			<li>
-				<a href="/" class:active={$page.url.pathname === '/'}>Home</a>
-			</li>
-			<li>
-				<a href="/invoices" class:active={$page.url.pathname === '/invoices'}>Invoices</a>
-			</li>
-			<li>
-				<a href="/repairs" class:active={$page.url.pathname === '/repairs'}>Repairs</a>
-			</li>
-			<li>
-				<a href="/account" class:active={$page.url.pathname === '/account'}>Account</a>
-			</li>
-		</ul>
+		<Menu {open} />
 	</nav>
 </header>
 
-<style lang="postcss">
-	nav ul li {
-		@apply mb-6;
-	}
-
-	nav ul li a {
-		@apply font-bold text-white hover:text-yellow-300 px-10;
-	}
-
-	nav ul li a.active {
-		background: url('/default-monochrome-black.svg') left top no-repeat;
-	}
-</style>
+<style lang="postcss"></style>
