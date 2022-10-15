e16f4614a64251c164b8b26e2fb90b09d9efacf5
Dorian C Brown <brown.3794@gmail.com>
2022-10-15 00:09:59 -0400

    feat: :sparkles: add archived to label  type

--- src/lib/components/Tag.svelte
+++ src/lib/components/Tag.svelte
@@ -15,7 +15,7 @@
   class:info={label === "draft"}
   class:success={label === "sent" || label === "current"}
   class:alert={label === "late"}
-  class:archive={label === "paid"}
+  class:archive={label === "paid" || label === 'archived'}
 >
   {label}
 </div>
