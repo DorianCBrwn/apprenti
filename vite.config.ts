10d58a782e604890be01b08264b2b2ae905ebab2
Dorian C Brown <brown.3794@gmail.com>
2022-10-20 21:20:44 -0400

    feat: :heavy_plus_sign: add vitest using svelte-adder

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,32 @@
-import { sveltekit } from "@sveltejs/kit/vite";
-import type { UserConfig } from "vite";
-import { Server } from "socket.io";
-
-// Socket Plugin Configuration
-export const webSocketServer = {
-  name: "webSocketServer",
-  configureServer(server) {
-    const io = new Server(server.httpServer);
-
-    io.on("connect", (socket) => {
-      socket.emit("eventFromServer", "Hello World");
-    });
-  },
-};
+import { sveltekit } from '@sveltejs/kit/vite';
+import type { UserConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
 
 const config: UserConfig = {
-  plugins: [sveltekit(), webSocketServer],
+  plugins: [sveltekit()],
+  define: {
+    // Eliminate in-source test code
+    'import.meta.vitest': 'undefined'
+  },
+  test: {
+    // jest like globals
+    globals: true,
+    environment: 'jsdom',
+    // in-source testing
+    includeSource: ['src/**/*.{js,ts,svelte}'],
+    // Add @testing-library/jest-dom matchers & setup MSW
+    setupFiles: ['./setupTest.js', './src/mocks/setup.ts'],
+    // Exclude files in c8
+    coverage: {
+      exclude: ['setupTest.js', 'src/mocks']
+    },
+    deps: {
+      // Put Svelte component here, e.g., inline: [/svelte-multiselect/, /msw/]
+      inline: [/msw/]
+    },
+    // Exclude playwright tests folder
+    exclude: [...configDefaults.exclude, 'tests']
+  }
 };
 
 export default config;
