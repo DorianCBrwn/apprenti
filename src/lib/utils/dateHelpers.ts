6995eccee48d9830f9cd0f162cfc6e8db061a314
Dorian C Brown <brown.3794@gmail.com>
2022-11-03 16:45:13 -0400

    refactor: :speech_balloon: remove spaces from date

--- src/lib/utils/dateHelpers.ts
+++ src/lib/utils/dateHelpers.ts
@@ -1,4 +1,4 @@
 export const formatDate = (dateString: string): string => {
   const [year, month, day] = dateString.split("-");
-  return `${month} / ${day} / ${year}`;
+  return `${month}/${day}/${year}`;
 };
