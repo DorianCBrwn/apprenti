5f21d10ccac1c567d7e8cf5c02561b92ebabdb9b
Dorian C Brown <brown.3794@gmail.com>
2022-11-10 15:19:12 -0500

    feat: :sparkles: add date and money helper functions

--- src/lib/utils/dateHelpers.ts
+++ src/lib/utils/dateHelpers.ts
@@ -1,4 +1,32 @@
+import dayjs from "dayjs";
+
+/**
+ * Takes a date string and returns a string formatted as mm/dd/yyyy
+ * @param  {string} dateString
+ * @returns {string}
+ */
 export const formatDate = (dateString: string): string => {
-  const [year, month, day] = dateString.split("-");
+  const [year, month, day] = splitDate(dateString);
   return `${month}/${day}/${year}`;
+};
+
+/**
+ * Takes a date string and returns an array.
+ * @param  {string} dateString
+ * @returns {string}
+ */
+export const splitDate = (dateString: string): string[] => {
+  return dateString.split("-");
+};
+
+/**
+ * Takes in a date and returns if the given date is later than today.
+ * @param  {string} dateString
+ * @returns {boolean}
+ */
+export const isLate = (dateString: string): boolean => {
+  const date = dayjs(dateString);
+  const today = dayjs();
+
+  return date.isBefore(today);
 };
--- src/lib/utils/moneyHelper.ts
+++ src/lib/utils/moneyHelper.ts
@@ -1,18 +1,52 @@
+/**
+ * Takes in an array of line items and returns a sum
+ * @param  {lineItem[]|undefined} lineItems
+ * @returns {number}
+ */
 export const sumLineItems = (lineItems: lineItem[] | undefined): number => {
-    if (!lineItems) return 0
-    return lineItems.reduce((preValue, currValue) => preValue + currValue.amount, 0);
-}
+  if (!lineItems) return 0;
+  return lineItems.reduce(
+    (preValue, currValue) => preValue + currValue.amount,
+    0
+  );
+};
 
+/**
+ * Takes in a number and returns it formatted as dollars and cents
+ * @param  {number} cents
+ * @returns {string}
+ */
 export const centsToDollars = (cents: number): string => {
-    const dollars = cents / 100;
-    const addDecimals = formatDecimals(dollars)
-    return addCommaSeparator(addDecimals)
-}
-
+  const dollars = cents / 100;
+  const addDecimals = formatDecimals(dollars);
+  return addCommaSeparator(addDecimals);
+};
+/**
+ * Takes in a number and returns a string representation of a number formatted to 2 decimal places
+ * @param  {number} num
+ * @returns {string}
+ */
 export const formatDecimals = (num: number): string => {
-    return num.toFixed(2)
-}
+  return num.toFixed(2);
+};
 
+/**
+ * adds a comma separator to string
+ * @param  {string} num
+ * @returns {string}
+ */
 export const addCommaSeparator = (num: string): string => {
-    return num.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-}
+  return num.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+/**
+ * takes in an array of invoices and returns the total sum of invoices.
+ * @param  {Invoice[]|undefined} invoices
+ * @returns {number}
+ */
+export const sumInvoices = (invoices: Invoice[] | undefined): number => {
+  if (!invoices) return 0;
+  return invoices.reduce((sum, num) => {
+    const invoiceSum = sumLineItems(num.lineItems);
+    return sum + invoiceSum;
+  }, 0);
+};
